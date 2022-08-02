f0a04caf7a9a02c8524183b6c00c5eb497fa3e3c
Alok Kumar <110452332+rajputalok1@users.noreply.github.com>
2022-08-02 13:44:46 +0530

    Update streamlit_app.py
    bug fixed so reverted some code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,16 @@
 import streamlit
-import pandas
 
 streamlit.title('My Moms New Healthy Dinner')
+
 streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Bolied Free-Range Egg')
 streamlit.text('🥑 Avacado Toast')
+
+streamlit.text('🥑 Build Your Own Fruit Smoothie')
+
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avacado','Strawberries'])
 streamlit.dataframe(my_fruit_list)
+
