11b017401856bf32ccc283f6292851bdf59868b2
Alok Kumar <110452332+rajputalok1@users.noreply.github.com>
2022-08-02 13:32:55 +0530

    Update streamlit_app.py
    importing CSV file data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit
+import pandas
+
 streamlit.title('My Moms New Healthy Dinner')
 streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Bolied Free-Range Egg')
 streamlit.text('🥑 Avacado Toast')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
